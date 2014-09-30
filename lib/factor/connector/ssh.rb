33dd26aceec46a7b829da0485b1d56f00956abd2
Maciej Skierkowski <maciej@skierkowski.com>
2014-09-30 16:33:41 -0700

    fixing bug with incorrect var ref

--- lib/factor/connector/ssh.rb
+++ lib/factor/connector/ssh.rb
@@ -158,7 +158,7 @@ Factor::Connector.service 'ssh' do
           end
         end
       end
-    rescue FactorChannelError
+    rescue Factor::Channel::Error
       raise
     rescue => ex
       fail "Couldn't connect to the server #{user}@#{host}:#{port || '22'}, please check credentials.", exception:ex
