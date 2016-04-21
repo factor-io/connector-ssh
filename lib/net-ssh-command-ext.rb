3495f990230334ac0e71e1825c61b641321be28b
Maciej Skierkowski <maciej@skierkowski.com>
2016-04-20 23:47:07 -0700

    refresh

--- lib/net-ssh-command-ext.rb
+++ lib/net-ssh-command-ext.rb
@@ -4,7 +4,7 @@ class Net::SSH::Connection::Session
   class CommandExecutionFailed < StandardError
   end
 
-  def exec_sc!(command)
+  def exec_sc!(command, &block)
     stdout_data,stderr_data = "",""
     exit_code,exit_signal = nil,nil
     self.open_channel do |channel|
@@ -13,18 +13,22 @@ class Net::SSH::Connection::Session
 
         channel.on_data do |_,data|
           stdout_data += data
+          block.call(:stdout, data) if block
         end
 
         channel.on_extended_data do |_,_,data|
           stderr_data += data
+          block.call(:stderr, data) if block
         end
 
         channel.on_request("exit-status") do |_,data|
           exit_code = data.read_long
+          block.call(:exit, exit_code) if block
         end
 
         channel.on_request("exit-signal") do |_, data|
           exit_signal = data.read_long
+          block.call(:exit_signal, exit_signal) if block
         end
       end
     end
